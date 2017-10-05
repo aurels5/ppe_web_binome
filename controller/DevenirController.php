1a40fe598607a8b01358bb50aae909d88d0099c5
sarradin <sarradin@DESKTOP-0O6K720>
2017-10-05 17:00:17 +0200

    new commit

--- controller/DevenirController.php
+++ controller/DevenirController.php
@@ -7,6 +7,7 @@
  * 
  * contrôleur pour le module devenir de l'application entreprises
  */
+include('verifications.php');
 
 class DevenirController extends Controller {
     
@@ -17,17 +18,22 @@ class DevenirController extends Controller {
     
     function ajouter_contact(){
         
-        if (is_null($this->modDevenir)) {
+        if (is_null($this->modDevenir)) { //charger le modèle Devenir
             $this->modDevenir = $this->loadModel('Devenir');
         }
         
-        $d['devenirs'] = $this->modDevenir->find(array('conditions' => 1));
+        if (is_null($this->modPromotion)) { //charger le modèle Promotion
+            $this->modPromotion = $this->loadModel('Promotion');
+        }
+        
+        $d['devenirs'] = $this->modDevenir->find(array('conditions' => 1)); //utilisé dans le foreach d'affichage
+        $d['promotions'] = $this->modPromotion->find(array('conditions' => 1));
         
         /*
         if (empty($d['devenir'])) {
             $this->e404('Page introuvable');
         }*/
-        
+
         $this->set($d);
     }
     
