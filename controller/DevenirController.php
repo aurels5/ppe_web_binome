64c5e28ba7aba2afc026ca1ec64180c1789fa656
sarradin <sarradin@DESKTOP-0O6K720>
2017-10-11 16:21:56 +0200

    new commit

--- controller/DevenirController.php
+++ controller/DevenirController.php
@@ -45,7 +45,12 @@ class DevenirController extends Controller {
         //faire un where :
         //$d['trucs'] = $this->modTruc->find(array('conditions' => array('codetruc'=>$codetruc, 'nomtruc'=>$nomtruc)   ));
         
+        //$d['eleves'] = $this->modEleve->find(array('conditions' => array('el_option'=>$opt, 'el_date_naissance'=>$el_date_nais)   ));
         
+        
+        //$d['eleves'] = $this->modEleve->find(array(
+        //   'conditions' => array('e_code' => $id)
+        //));
         
         /*
         if (empty($d['devenir'])) {
@@ -61,6 +66,66 @@ class DevenirController extends Controller {
     }
     
     
+    function modifier_contact($id){//paramètre $id
+        
+        if (is_null($this->modDevenir)) { //charger le modèle Devenir
+            $this->modDevenir = $this->loadModel('Devenir');
+        }
+        
+        if (is_null($this->modPromotion)) { //charger le modèle Promotion
+            $this->modPromotion = $this->loadModel('Promotion');
+        }
+        
+        if (is_null($this->modUser)) { //charger le modèle Eleve
+            $this->modUser = $this->loadModel('User');
+        }
+        
+        if (is_null($this->modEleve)) { //charger le modèle Eleve
+            $this->modEleve = $this->loadModel('Eleve');
+        }
+        
+        $d['devenirs'] = $this->modDevenir->find(array('conditions' => 1)); //utilisé dans le foreach d'affichage
+        $d['promotions'] = $this->modPromotion->find(array('conditions' => 1));
+        $d['eleves'] = $this->modEleve->find(array('conditions' => 1));
+        $d['users'] = $this->modUser->find(array('conditions' => 1));
+        
+        
+        /*
+        $code_truc= $id[0];
+        $modTruc= $this->loadModel('Truc');
+        $donnees=array();
+        $donnees['nomtruc']=$_POST['nomtruc'];
+        $donnees['codetruc']=$_POST['codetruc'];
+        $donnees['autretruc']=$_POST['autretruc'];
+        $donnees['encoretruc']=$_POST['encoretruc'];
+        
+        $clef=array();
+        $clef['codetruc']=$codetruc;
+        
+        $tab=array();
+        $tab['clef']=$clef;
+        $tab['donnees']=$donnees;
+        
+        $modTruc->update($tab);
+        
+        $d['truc']=donnees;
+        $this->set($d);
+         * 
+         */
+    }
+    
+    
+    //méthode déjà dans la classe mère, contenu différent
+    //pour ajouter un contact on utilise la même vue que pour modifier un contact
+    //l'intérêt de modifier la méthode fille render est de ne pas réécrire une vue
+    function render($view) {
+        if($view=='modifier_contact'){
+            $view='ajouter_contact';
+        }
+        else{
+            parent::render($view); //$view='ajouter_contact' par exemple
+        }
+    }
     
     
 }
