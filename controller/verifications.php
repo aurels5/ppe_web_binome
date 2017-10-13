0da84ff2c8d0c763aba3b6ce6d9accfde76568d7
sarradin <sarradin@DESKTOP-0O6K720>
2017-10-13 10:19:55 +0200

    new commit

--- controller/verifications.php
+++ controller/verifications.php
@@ -1,8 +1,33 @@
 <?php
 
-    function dateShowYear($date){
-        $tab_date=explode('-',$date);
-        $year=intval($tab_date[0]);
-        return $year;
+function dateShowYear($date){//méth. qui permet de montrer l'année d'un 2017-05-01 : renvoie un int 2017
+    $tab_date=explode('-',$date);
+    $year=intval($tab_date[0]);
+    return $year;
+}
+
+    
+function verifDateY_m_d($date){//méth. pour vérifier validité date fiche contact
+    $valid=TRUE; //initialisation   
+    //on explose la chaîne avec le délimiteur -
+    $tableau_date=explode('-',$date);
+    list($annee, $mois, $jour)=explode('-',$date);
+    if($tableau_date=='' || $annee<=1930 || ($annee>date('Y')) || $mois<1 || $mois>12 || $jour<1 || $jour>31){
+        $valid=FALSE;
     }
+    else{
+        $nb_indices_tab=count($tableau_date);
+        //on teste si le nombre d'indices est bien égal à 3
+        if ($nb_indices_tab <> 3) {
+            $valid=FALSE;
+        }
+    }  
+    if ($valid){//==TRUE
+        $d = DateTime::createFromFormat('Y-m-d', $date);
+        return $d && $d->format('Y-m-d') === $date;
+    }
+    else {
+        throw new Exception('Date incorrecte : ' . $date);
+    }
+}
     
